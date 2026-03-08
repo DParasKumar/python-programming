5778593e69a7f1db28901f5fb9d42e9ec3b665a2
D Paras Kumar <paraskumar475@gmail.com>
2026-03-08 15:57:24 +0530

    changes

--- Working with Variables in Python to Manage Data/what is your name.py
+++ Working with Variables in Python to Manage Data/what is your name.py
@@ -1,2 +1,13 @@
-name = input("what is your name:\n")
-print("so your name is",name)
+# name = input("what is your name:\n")
+# print("so your name is",name)
+
+name = "Jack"
+print(name)
+
+name = "Angela"
+print(name)
+
+print()
+name = input("what is your name?")
+length = len(name)
+print(length)
