3a8d77cff827fe38b8bb74a7b2d089db0f05c7fd
D Paras Kumar <paraskumar475@gmail.com>
2026-03-08 17:25:57 +0530

    update

--- Working with Variables in Python to Manage Data/strings.py
+++ Working with Variables in Python to Manage Data/strings.py
@@ -1,2 +1,8 @@
 print("Hello world !\nHello world") #create a new line
 print("Hello world !\nHello world\nanother line")
+
+# concatinating.
+
+print("Hello" + " Paras")
+print("Hello " + "Paras")
+print("Hello" + " " + "Paras")
