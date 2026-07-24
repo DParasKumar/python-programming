347145d502ee0cd2943d5c973c571003d846fd9b
D Paras Kumar <paraskumar475@gmail.com>
2026-07-24 20:56:46 +0530

    input exercise

---
+++ Working with Variables in Python to Manage Data/input exercise.py
@@ -0,0 +1,6 @@
+#   INSTRUCTIONS
+# write a program that prints the number of characters in a user's name. You might need to Google for a fucntion that calculates the length of a string.
+#WARNING. Your program should work for a different inputs as well like for any name that you input.
+
+name = (input("Please Enter Your name:"))
+print(len(name))
