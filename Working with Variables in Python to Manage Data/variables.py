37ba87c7e3086d35ab7dbbdd9abc4915113d0ee7
D Paras Kumar <paraskumar475@gmail.com>
2026-07-24 22:44:47 +0530

    variable practice

---
+++ Working with Variables in Python to Manage Data/variables.py
@@ -0,0 +1,6 @@
+name = input("what is your name?")
+# name is a variable.
+print(name)
+
+name = "jack"
+print(name)
---
+++ Working with Variables in Python to Manage Data/varialbe practice.py
@@ -0,0 +1,10 @@
+#INSTRUCTIONS
+#Write a program that switches the values stored in the variables a and b.
+#Warning. Do not change the code on lines 1-4 and 15-18. Your Program should work for different inputs. any value of a and b.
+a = 100
+b = 200
+temp = a
+a=b
+b=temp
+print(a)
+print(b)
