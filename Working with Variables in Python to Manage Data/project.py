92e6c88a47bcfaec9b4f583219efc311d5efea11
D Paras Kumar <paraskumar475@gmail.com>
2026-07-28 03:16:55 +0530

    first project

---
+++ Working with Variables in Python to Manage Data/project.py
@@ -0,0 +1,9 @@
+# create a greeting for your program.
+print("welcome to the band name project")
+#ask the user for the city that they grew up in.
+city = (input("What is your city name that you grew in?:"))
+#ask the user for the name of a pet.
+pet = (input("what is the name of your pet?:"))
+#combine the name of their city and pet and show them their band name.
+print("Your band name coudl be " + city + " " + pet  )
+#make sure the input cursor shows on a new line
