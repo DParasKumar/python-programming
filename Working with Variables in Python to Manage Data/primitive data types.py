9d4631160c5a88b282a2e3c48e42be352387e34f
D Paras Kumar <paraskumar475@gmail.com>
2026-08-01 02:24:23 +0530

    primitive data types

---
+++ Working with Variables in Python to Manage Data/primitive data types.py
@@ -0,0 +1,31 @@
+#data types
+
+#strings
+
+print("Hello"[0])
+print("Hello"[1])
+print("Hello"[2])
+print("Hello"[3])
+print("Hello"[4])
+# This is called subscripting
+# word = "Hello"
+# for text in word:
+#     print(text)
+
+print("123" + "345")
+
+print("hello" + "world")
+
+# Integer
+
+print(123 + 345)
+
+123_456_789
+
+# float
+3.14159
+
+# boolean
+
+True
+False
